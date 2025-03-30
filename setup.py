e2e4f515a0253aeb39beacb1e383e188a7809caf
Erim Yanik <erimyanik@Erims-MacBook-Pro.local>
2025-03-30 13:13:57 -0400

    Added matplotlib as dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='trustquant',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=['numpy', 'scikit-learn'],
+    install_requires=['numpy', 'scikit-learn', 'matplotlib'],
     author='Erim_Yanik',
     description='Trustworthiness metrics for Softmax predictive models',
     url='https://github.com/yaniker/trust-quantification-metrics-for-deep-neural-networks-python',
