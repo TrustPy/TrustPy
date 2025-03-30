2cc08320860cd0b66bf659dd457fedcbce473ef1
Erim Yanik <erimyanik@Erims-MacBook-Pro.local>
2025-03-29 23:43:59 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trustquant',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['numpy', 'scikit-learn'],
+    author='Erim_Yanik',
+    description='Trustworthiness metrics for Softmax predictive models',
+    url='https://github.com/yaniker/trust-quantification-metrics-for-deep-neural-networks-python',
+    license='MIT',
+)
