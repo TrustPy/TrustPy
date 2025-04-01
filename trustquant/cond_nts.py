bce911aa04682e0a58aa276c742580522e0c010f
Erim Yanik <ey23@h22-login-24.rcc.fsu.edu>
2025-04-01 16:12:50 -0400

    init commit

--- trustquant/cond_nts.py
+++ trustquant/cond_nts.py
@@ -54,10 +54,10 @@ class CNTS:
         # Construct the dictionary
         nts_dict = {}
         for i in range(n_classes):
-            nts_dict[f'class_{i}_nts'] = class_nts[i]
-            nts_dict[f'class_{i}_nts_correct'] = cond_nts_correct[i]
-            nts_dict[f'class_{i}_nts_incorrect'] = cond_nts_incorrect[i]
-        nts_dict['overall_nts'] = overall_nts
+            nts_dict[f'class_{i}'] = f'{class_nts[i]:.3f}'
+            nts_dict[f'class_{i}_correct'] = f'{cond_nts_correct[i]:.3f}'
+            nts_dict[f'class_{i}_incorrect'] = f'{cond_nts_incorrect[i]:.3f}'
+        nts_dict['overall'] = f'{overall_nts:.3f}'
 
         return nts_dict
 
@@ -98,7 +98,7 @@ class CNTS:
         for i in range(self.oracle.shape[0]):
             true_label = self.oracle[i]
             pred_label = predicted_class[i]
-            max_prob 현실 = self.predictions[i, pred_label]
+            max_prob = self.predictions[i, pred_label]
             if pred_label == true_label:
                 correct_trust[true_label].append(max_prob**self.alpha)
             else:
@@ -140,7 +140,7 @@ class CNTS:
             x_range (np.ndarray): X-axis values for density curves.
             n_classes (int): Number of classes.
         """
-        class_labels = [f'{explanationClass {i}' for i in range(n_classes)]
+        class_labels = [f'CLass {i}' for i in range(n_classes)]
         colors = plt.cm.tab10(np.arange(n_classes))
         fig, ax = plt.subplots(figsize=(6 * n_classes, 6), ncols=n_classes, sharey=True)
         if n_classes == 1:
